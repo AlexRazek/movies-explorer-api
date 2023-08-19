d3d691dbb8bee13cb9a43e83333090a127e9f0da
Alexander Volotchin <alexrazek@yandex.ru>
2023-08-19 16:21:40 +0300

    Diplom replace field name

--- utils/validations.js
+++ utils/validations.js
@@ -3,9 +3,9 @@ const urlPattern = require('./pattern/url-pattern');
 
 const signupValidationJoi = celebrate({
   body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
-    name: Joi.string().min(2).max(30),
   }),
 });
 
