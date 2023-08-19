0bd04ea2cc965c8f254a9aa305c8243fac1aceac
Alexander Volotchin <alexrazek@yandex.ru>
2023-08-19 19:20:23 +0300

    Diplom change sameSite - lax

--- controllers/users.js
+++ controllers/users.js
@@ -92,7 +92,7 @@ const login = (req, res, next) => {
           httpOnly: true,
           // sameSite: true,
           // используется none т.к. не проходят куки с сервера ВМ на localHost
-          sameSite: 'none',
+          sameSite: 'lax',
         });
       // .end(); // если у ответа нет тела, можно использовать метод end
       // вернём токен
