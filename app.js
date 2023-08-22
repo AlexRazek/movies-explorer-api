8b026a95a5cefd7574b67c6bd705a1ddd377e234
Alexander Volotchin <alexrazek@yandex.ru>
2023-08-23 02:16:16 +0300

    Diplom change cors

--- app.js
+++ app.js
@@ -12,18 +12,6 @@ const { apiLimiter } = require('./middlewares/apiLimiter');
 const app = express();
 const { generalRoutes } = require('./routes/index');
 
-app.use(cors({
-  // origin: allowedCors,
-  origin: true,
-  // origin: 'http://localhost:3000',
-  credentials: true,
-}));
-
-const { PORT, MONGO_URI } = process.env;
-// const { PORT = 3000, MONGO_URI = 'mongodb://localhost:27017' } = process.env;
-
-app.use(cookieParser());
-
 const allowedCors = [
   'https://praktikum.tk',
   'http://praktikum.tk',
@@ -39,34 +27,61 @@ const allowedCors = [
   // 'https://alexmesto.nomoredomains.work',
 ];
 
-app.use((req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Origin', origin);
-    // res.header('Access-Control-Allow-Credentials', true);
-  }
+app.use(cors({
+  origin: allowedCors,
+  // origin: true,
+  // origin: 'http://localhost:3000',
+  credentials: true,
+}));
 
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+const { PORT, MONGO_URI } = process.env;
+// const { PORT = 3000, MONGO_URI = 'mongodb://localhost:27017' } = process.env;
 
-  // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-  }
+app.use(cookieParser());
 
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
-  }
-  return next();
-});
+// const allowedCors = [
+//   'https://praktikum.tk',
+//   'http://praktikum.tk',
+//   'http://localhost:3000',
+//   'https://localhost:3000',
+//   'localhost:3000',
+//   'http://127.0.0.1:3000',
+//   'http://api.alexmovie.nomoredomains.xyz',
+//   'https://api.alexmovie.nomoredomains.xyz',
+//   'http://api.nomoreparties.co',
+//   'https://api.nomoreparties.co',
+//   // 'http://alexmesto.nomoredomains.work',
+//   // 'https://alexmesto.nomoredomains.work',
+// ];
+
+// app.use((req, res, next) => {
+//   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+//   // проверяем, что источник запроса есть среди разрешённых
+//   if (allowedCors.includes(origin)) {
+//     // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     res.header('Access-Control-Allow-Origin', origin);
+//     // res.header('Access-Control-Allow-Credentials', true);
+//   }
+
+//   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+//   Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+//   // Если это предварительный запрос, добавляем нужные заголовки
+//   if (method === 'OPTIONS') {
+//     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//   }
+
+//   const requestHeaders = req.headers['access-control-request-headers'];
+//   if (method === 'OPTIONS') {
+//     // разрешаем кросс-доменные запросы с этими заголовками
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     // завершаем обработку запроса и возвращаем результат клиенту
+//     return res.end();
+//   }
+//   return next();
+// });
 
 app.get('/crash-test', () => {
   setTimeout(() => {
