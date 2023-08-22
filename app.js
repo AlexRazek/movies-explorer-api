be473583363a2f6ae8c698f555cc68bfa6c79610
Alexander Volotchin <alexrazek@yandex.ru>
2023-08-23 01:59:39 +0300

    Diplom change cors

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ const { generalRoutes } = require('./routes/index');
 app.use(cors({
   // origin: allowedCors,
   // origin: true,
-  // origin: 'http://localhost:3000',
-  // credentials: true,
+  origin: 'http://localhost:3000',
+  credentials: true,
 }));
 
 const { PORT, MONGO_URI } = process.env;
@@ -43,8 +43,8 @@ app.use((req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    // res.header('Access-Control-Allow-Origin', origin);
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
   }
 
