c520a011819f458491241c4940d30d114b1cd886
Alexander Volotchin <alexrazek@yandex.ru>
2023-08-23 00:53:31 +0300

    Diplom change cors

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ const app = express();
 const { generalRoutes } = require('./routes/index');
 
 app.use(cors({
-  // origin: true,
-  origin: 'http://localhost:3000',
+  origin: true,
+  // origin: 'http://localhost:3000',
   credentials: true,
 }));
 
@@ -43,6 +43,7 @@ app.use((req, res, next) => {
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
     // res.header('Access-Control-Allow-Origin', '*');
   }
 
