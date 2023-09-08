3af3fd2bd107ccec4afdb40376de5e218ca79f45
Alexander Volotchin <alexrazek@yandex.ru>
2023-09-08 11:46:20 +0300

    Diplom edit after final edit cookie

--- controllers/users.js
+++ controllers/users.js
@@ -95,7 +95,6 @@ const login = (req, res, next) => {
           sameSite: 'None',
           secure: true,
           path: '/',
-          domain: 'nomoredomains.xyz',
         });
       // .end(); // если у ответа нет тела, можно использовать метод end
       // вернём токен
